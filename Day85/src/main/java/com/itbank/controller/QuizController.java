accce350c86b2d04b77cdd0251ea0135a66038ee
itbank <itbank@DESKTOP-7EQF3M8>
2023-11-15 09:13:37 +0900

    1115_0913

--- Day85/src/main/java/com/itbank/controller/QuizController.java
+++ Day85/src/main/java/com/itbank/controller/QuizController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itbank.model.dto.BoardDTO;
+
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
 
 	@GetMapping
 	public List<BoardDTO> quiz() {
-		
+		return null;
 	}
 }
